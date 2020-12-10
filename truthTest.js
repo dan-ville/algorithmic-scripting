f4d6b8cc75894fa624f9483409250b6583981226
Daniel Villegas <d.villegas@visiontravel.net>
2020-12-10 15:07:58 -0500

    truthTest.js

---
+++ truthTest.js
@@ -0,0 +1,18 @@
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+function findElement(arr, func) {
+
+    for (let el of arr) {
+       if (func(el) === true) {
+        return el
+      }
+    }
+  
+    return undefined
+}
+  
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
+// should return 8.
+
+findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; });
+// should return undefined. 
